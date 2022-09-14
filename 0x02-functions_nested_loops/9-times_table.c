afb6832d9bc0d2ddd62115deacc896c08cd26707
marymativo <marymativo850@gmail.com>
2022-09-14 12:25:40 -0700

    coding

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,32 +1,76 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * times_table - Prints the 9 times table, starting with 0.
+ * times_table - Print the 9 times table, starting with 0
+ * Return: 0
  */
+
 void times_table(void)
 {
-	int num, mult, prod;
+	int a = 0; /* factor */
+	int b; /* count  */
+	int c; /* computed value */
 
-	for (num = 0; num <= 9; num++)
+	while (a < 10)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+		b = 0;
+		while (b < 10)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			if (prod <= 9)
+			c = a * b;
+			if (c > 9)
+			{
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+			else if (b != 0)
+			{
 				_putchar(' ');
+				_putchar(c + '0');
+			}
 			else
-				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
+			{
+				_putchar(c + '0');
+			}
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			b++;
 		}
 		_putchar('\n');
+		a++;
 	}
 }
-Footer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
