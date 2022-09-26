147ed4186fff67c723816fa10905fa6a967edcd8
marymativo <marymativo850@gmail.com>
2022-09-26 07:13:56 -0700

    coding

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,27 +1,49 @@
 #include "main.h"
 
 /**
- * _memcpy - copies n bytes from src to dest buffers
- * @dest: Destination buffer
- * @src: Source buffer
- * @n: Number of bytes to copy from @src to @dest
+ * _memset - fills a memory buffer with a constant byte/character
+ * @s: Pointer to the byte buffer
+ * @b: Constant byte
+ * @n: Number of buffer spaces to fill
  *
- * Return: @dest
+ * Return: @s
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	i = 0;
 	while (i < n)
 	{
-		*(dest + i) = *(src + i);
-
+		*(s + i) = b;
 		i++;
 	}
 
-	return (dest);
+	return (s);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
